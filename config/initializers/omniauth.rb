171a16e710ef05a44b3116319e62258cb49015a4
Wouter de Vos <wouter@surecreations.com>
2012-10-16 21:36:29 +0200

    Some minor fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "gist"
+  provider :github, ENV['CODEZIT_GITHUB_KEY'], ENV['CODEZIT_GITHUB_SECRET'], scope: "gist"
 end
