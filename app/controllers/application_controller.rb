b2fc0c793670d27b0e2b23f71f0ba719ba1fe2f0
Wouter de Vos <wouter@surecreations.com>
2012-10-18 08:29:05 +0200

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,12 @@ class ApplicationController < ActionController::Base
   before_filter :check_for_subdomain
 
   def check_for_subdomain
-    if (request.subdomain.empty? || request.subdomain == "www") && user_signed_in?
-      if current_user.account
+    if (request.subdomain.empty? || request.subdomain == "www") &&
+      user_signed_in? && current_user.account
         redirect_to(
           (root_url subdomain: current_user.account.slug) +
           request.path.sub(/^\//, '')
         )
-      end
     end
   end
 end
