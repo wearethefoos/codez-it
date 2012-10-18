fba50f885b31f182b85c023fdc2fd378abc5338d
Wouter de Vos <wouter@surecreations.com>
2012-10-18 08:30:51 +0200

    random test order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  # config.order = "random"
+  config.order = "random"
 
   # Clean up the database
   require 'database_cleaner'
